e91533e56687855835041ade5c529e656f60ba57
Nathan Taylor <nat986@gmail.com>
2015-11-20 10:27:34 -0500

    Added views to create/edit feedback from inside site.

--- feedback/urls.py
+++ feedback/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.feedback_list, name='feedback_list'),
     url(r'^feedback/(?P<pk>[0-9]+)/$', views.feedback_detail, name='feedback_detail'),
+    url(r'^post/new/$', views.feedback_new, name='feedback_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.feedback_edit, name='feedback_edit'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Feedback
+from .forms import FeedbackForm
 # Create your views here.
 
 def feedback_list(request):
@@ -10,3 +11,30 @@ def feedback_list(request):
 def feedback_detail(request, pk):
     feedback = get_object_or_404(Feedback, pk=pk)
     return render(request, 'feedback/feedback_detail.html', {'feedback': feedback})
+
+def feedback_new(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.author = request.user
+            feedback.published_date = timezone.now()
+            feedback.save()
+            return redirect('feedback.views.feedback_detail', pk=feedback.pk)
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback/feedback_edit.html', {'form': form})
+
+def feedback_edit(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    if request.method == "POST":
+        form = FeedbackForm(request.POST, instance=feedback)
+        if form.is_valid():
+            feedback = form.save(commit=False)
+            feedback.author = request.user
+            feedback.published_date = timezone.now()
+            feedback.save()
+            return redirect('feedback.views.feedback_detail', pk=feedback.pk)
+    else:
+        form = FeedbackForm(instance=feedback)
+    return render(request, 'feedback/feedback_edit.html', {'form': form})
