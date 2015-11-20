5f7104372af53baa27855f4f099a8fde0be638a2
Nathan Taylor <nat986@gmail.com>
2015-11-20 09:54:52 -0500

    Detail View.

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.feedback_list, name='feedback_list'),
+    url(r'^feedback/(?P<pk>[0-9]+)/$', views.feedback_detail, name='feedback_detail'),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Feedback
 # Create your views here.
@@ -6,3 +6,7 @@ from .models import Feedback
 def feedback_list(request):
     feedbacks = Feedback.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'feedback/feedback_list.html', {'feedbacks':feedbacks})
+
+def feedback_detail(request, pk):
+    feedback = get_object_or_404(Feedback, pk=pk)
+    return render(request, 'feedback/feedback_detail.html', {'feedback': feedback})
